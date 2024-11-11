c44fb1014df64858949e3ef2a1d58d278c7e8d3f
tom-huntington <55266932+tom-huntington@users.noreply.github.com>
2024-11-11 19:25:19 +1300

    Add project files.

---
+++ SetWindowLongMRE/MainWindow.xaml.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+#include <microsoft.ui.xaml.window.h>
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::SetWindowLongMRE::implementation
+{
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        winrt::Microsoft::UI::Xaml::Window window{ *this };
+        winrt::com_ptr<IWindowNative> windowNative = window.as<IWindowNative>();
+        HWND hwnd{};
+        windowNative->get_WindowHandle(&hwnd);
+        SetWindowLong(hwnd, GWL_STYLE, WS_OVERLAPPEDWINDOW & ~(WS_CAPTION | WS_MAXIMIZEBOX | WS_MINIMIZEBOX | WS_SYSMENU | WS_THICKFRAME));
+        ShowWindow(hwnd, SW_MAXIMIZE);
+        myButton().Content(box_value(L"Clicked"));
+    }
+}
